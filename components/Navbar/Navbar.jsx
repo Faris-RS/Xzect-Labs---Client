6d2cc6215fcb39893741770e659a2ebf48d17ee2
Mohamed Faris <brotofarisrs@gmail.com>
2023-07-14 11:54:05 +0530

    header

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <div id="stripes">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="header">
+        <div class="nine">
+          <h1>
+            Maktub<span>Your Daily Log</span>
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
